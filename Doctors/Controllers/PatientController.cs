7749b01e034571bc372552ba4381ee9002643240
Ahmed Khokar and Derek Smith <derek@>
2018-09-25 09:44:47 -0700

    init setup

---
+++ Doctors/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collection.Generic;
+using Doctors.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doctor.Controllers
+{
+    public class PatientController : Controller
+    {
+      
+    }
+}
---
+++ Doctors/Models/Doctor.cs
@@ -0,0 +1,4 @@
+using System;
+using System.Collection.Generic;
+using MySql.Data.MySqlClient;
+using Doctor;
